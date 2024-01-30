8b449bc1bb3432de7d01495822b2e849b87277c1
Naga-saipavan <96619322+Naga-saipavan@users.noreply.github.com>
2024-01-30 16:33:39 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,6 +1,9 @@
 function addToCart(title, price) {
     let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    
     cartItems.push({ title, price });
+    
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
